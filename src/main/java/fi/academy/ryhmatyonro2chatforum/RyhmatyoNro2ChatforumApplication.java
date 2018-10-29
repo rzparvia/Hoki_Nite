a016dd769e3c1b1322bea639592f21bfd9ab606d
rzparvia <arno.p.parviainen@gmail.com>
2018-10-29 16:07:32 +0200

    Eka pushi

---
+++ src/main/java/fi/academy/ryhmatyonro2chatforum/RyhmatyoNro2ChatforumApplication.java
@@ -0,0 +1,12 @@
+package fi.academy.ryhmatyonro2chatforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RyhmatyoNro2ChatforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RyhmatyoNro2ChatforumApplication.class, args);
+    }
+}
---
+++ src/test/java/fi/academy/ryhmatyonro2chatforum/RyhmatyoNro2ChatforumApplicationTests.java
@@ -0,0 +1,16 @@
+package fi.academy.ryhmatyonro2chatforum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RyhmatyoNro2ChatforumApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
