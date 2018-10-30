a97126327a2f94104edc1bc75fa070d28b5c8bee
rzparvia <arno.p.parviainen@gmail.com>
2018-10-30 16:53:05 +0200

    Vittu

--- src/main/java/fi/academy/ryhmatyonro2chatforum/RestiKontrolleri.java
+++ src/main/java/fi/academy/ryhmatyonro2chatforum/RestiKontrolleri.java
@@ -2,28 +2,26 @@ package fi.academy.ryhmatyonro2chatforum;
 
 import fi.academy.ryhmatyonro2chatforum.entities.Messages;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/index")
-public class RestiKontrolleri {
+class RestiKontrolleri {
     private UserRepo userRepo;
     private MessageRepo messageRepo;
     private SectionRepo sectionRepo;
     private TopicRepo topicRepo;
 
     @Autowired
-    public RestiKontrolleri(UserRepo userRepo, MessageRepo messageRepo, SectionRepo sectionRepo, TopicRepo topicRepo) {
+    RestiKontrolleri(UserRepo userRepo, MessageRepo messageRepo, SectionRepo sectionRepo, TopicRepo topicRepo) {
         this.userRepo = userRepo;
         this.messageRepo = messageRepo;
         this.sectionRepo = sectionRepo;
         this.topicRepo = topicRepo;
     }
 
-    @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public Iterable<Messages> kaikkiViestit() {
+    @GetMapping("/all")
+    Iterable<Messages> all() {
         return messageRepo.findAll();
     }
 }
